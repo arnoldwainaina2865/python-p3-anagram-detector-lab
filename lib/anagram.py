5546ba33e52f0e4b65947c9f2b88cfb22ee6830b
Arnold Wainaina <arnoldwainaina2865@gmail.com>
2025-06-04 17:17:46 +0300

    initial commit

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,15 @@
-# your code goes here!
+# lib/anagram.py
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, word_list):
+        matches = []
+        sorted_original = sorted(self.word)
+
+        for w in word_list:
+            if sorted(w) == sorted_original:
+                matches.append(w)
+
+        return matches
